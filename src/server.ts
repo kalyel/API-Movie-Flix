d304f7925f50b0ad6cf4b3b67cee280980438362
Kalyel Mendes <kalyelmendesdapaixao@yahoo.com.br>
2026-02-21 14:16:33 -0300

    chore: configurações express

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,12 @@
-const teste: string = 'assaassa';
-console.log(teste);
+import express from 'express';
+
+const port = 3000;
+const app = express();
+
+app.get('/movies', (req, res) => {
+  res.send('Listagem de filmes');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor em execução na porta http://localhost:${port}`);
+});
