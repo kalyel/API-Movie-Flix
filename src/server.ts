44bc5eb083b9a87d68cb43e0a65fb7b97a2db3a7
Kalyel Mendes <kalyelmendesdapaixao@yahoo.com.br>
2026-02-27 00:28:34 -0300

    feat: adicinado o filtro de filmes por gênero

--- src/server.ts
+++ src/server.ts
@@ -95,6 +95,29 @@ app.delete('/movies/:id', async (req, res) => {
   res.status(200).send();
 });
 
+app.get('/movies/:genreName', async (req, res) => {
+  try {
+    const moviesFilteredByGenreName = await prisma.movie.findMany({
+      include: {
+        genres: true,
+        languages: true,
+      },
+      where: {
+        genres: {
+          name: {
+            equals: req.params.genreName,
+            mode: 'insensitive',
+          },
+        },
+      },
+    });
+
+    res.status(200).send(moviesFilteredByGenreName);
+  } catch (error) {
+    res.status(500).send({ message: 'Falha ao filtrar filmes por gênero' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Servidor em execução na porta http://localhost:${port}`);
 });
